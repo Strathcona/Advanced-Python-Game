6cf13add5915370ea322feb8bdd00c82b55a1bda
Strathcona <flymancan@gmail.com>
2020-09-03 15:54:41 -0400

    Sword and Animations

--- sword.py
+++ sword.py
@@ -4,7 +4,7 @@ class Sword(GameObject.GameObject):
     #If we are currently swinging
     swinging = False
     #How long the sword swings for
-    swingTime = 10
+    swingTime = 9
     swingTimer = 0
     
     def __init__(self, game):
@@ -13,25 +13,65 @@ class Sword(GameObject.GameObject):
         self.damage = 1
         self.solid = False
         self.animationFrames = {
-                        "SwordU":pygame.image.load("Images/SwordU.png"),
-                        "SwordD":pygame.image.load("Images/SwordD.png"),
-                        "SwordL":pygame.image.load("Images/SwordL.png"),
-                        "SwordR":pygame.image.load("Images/SwordR.png"),
+                        "SwordU1":pygame.image.load("Images/SwordU1.png"),
+                        "SwordU2":pygame.image.load("Images/SwordU2.png"),
+                        "SwordU3":pygame.image.load("Images/SwordU3.png"),
+
+                        "SwordD1":pygame.image.load("Images/SwordD1.png"),
+                        "SwordD2":pygame.image.load("Images/SwordD2.png"),
+                        "SwordD3":pygame.image.load("Images/SwordD3.png"),
+                        
+                        "SwordL1":pygame.image.load("Images/SwordL1.png"),
+                        "SwordL2":pygame.image.load("Images/SwordL2.png"),
+                        "SwordL3":pygame.image.load("Images/SwordL3.png"),
+                        
+                        "SwordR1":pygame.image.load("Images/SwordR1.png"),
+                        "SwordR2":pygame.image.load("Images/SwordR2.png"),
+                        "SwordR3":pygame.image.load("Images/SwordR3.png"),
                         }
+        self.animations = {"SwordU":["SwordU1","SwordU2","SwordU3"],
+                           "SwordD":["SwordD1","SwordD2","SwordD3"],
+                           "SwordR":["SwordR1","SwordR2","SwordR3"],
+                           "SwordL":["SwordL1","SwordL2","SwordL3"]
+                           }
         #Hide our sword at the start!
+        self.currentAnimation = "SwordU1"
+        #keep count of frames for animation
+        self.frameCounter = 0
+        #current animation frame is what image in the animation we are on
+        self.currentAnimationFrame = 0
+        #What direction our character is facing
         self.sprite.kill()
 
+    def SetAnimation(self, animationName):
+        if animationName in self.animations and self.currentAnimation != animationName:
+            self.currentAnimation = animationName
+            self.currentAnimationFrame = 0
+            self.frameCounter = 0
+        
+    def Animate(self):
+        framesPerAnimation = 3
+        if(self.frameCounter > framesPerAnimation):
+            self.currentAnimationFrame += 1
+            self.frameCounter = 0
+        self.frameCounter += 1
+        #Reset our animation frame back to 0 if we reach the end of the animation
+        if self.currentAnimationFrame > (len(self.animations[self.currentAnimation]) - 1):
+            self.currentAnimationFrame = 0
+            
+        #animation frames takes a string from animations indexed by current frame to produce an image
+        self.sprite.image = self.animationFrames[self.animations[self.currentAnimation][self.currentAnimationFrame]]
     
     def Use(self, facing):
         #Start swinging the sword
         if facing == "Up":
-            self.sprite.image = self.animationFrames["SwordU"]
+            self.SetAnimation("SwordU")
         if facing == "Down":
-            self.sprite.image = self.animationFrames["SwordD"]
+            self.SetAnimation("SwordD")
         if facing == "Left":
-            self.sprite.image = self.animationFrames["SwordL"]
+            self.SetAnimation("SwordL")
         if facing == "Right":
-            self.sprite.image = self.animationFrames["SwordR"]
+            self.SetAnimation("SwordR")
 
         self.swinging = True
         self.swingTimer = self.swingTime
@@ -39,6 +79,8 @@ class Sword(GameObject.GameObject):
         
     def Update(self):
         if self.swinging:
+            self.Animate()
+
             collisions = self.game.GetCollision(self.sprite.rect, "Moblin")
             for collision in collisions:
                 collision.TakeDamage(self.damage)
